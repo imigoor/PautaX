4ec59cc6573043ed4efd60724b7470450c11da3c
Flavio <flaflagi@gmail.com>
2025-11-02 12:27:35 -0300

    feat: Implementa Use Cases do CRUD de Colegiado

---
+++ src/main/java/br/edu/ifpb/pautax/application/useCases/administrador/colegiados/cadastrar/SalvarColegiadoUseCase.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.pautax.application.useCases.administrador.colegiados.cadastrar;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.ifpb.pautax.domain.entities.Colegiado;
+import br.edu.ifpb.pautax.infrastructure.repositories.ColegiadoRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SalvarColegiadoUseCase implements ISalvarColegiadoUseCase {
+    private final ColegiadoRepository colegiadoRepository;
+
+    @Override
+    public String execute(Colegiado colegiado) {
+        colegiadoRepository.save(colegiado);
+        return "redirect:/administrador/colegiados";
+    }
+}
