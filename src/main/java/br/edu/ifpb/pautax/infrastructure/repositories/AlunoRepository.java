5b683823cb133463c2ca62adc02c2672d843ad0f
Gustavo Gabriel Batista <gugagabriel56@gmail.com>
2025-11-11 22:25:48 -0300

    Refactor: Retirando métodos que não estavam sendo utilizados

--- src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/AlunoRepository.java
+++ src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/AlunoRepository.java
@@ -9,8 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
-    boolean existsByUsuarioLogin(String login);
     Optional<Aluno> findByUsuario(Usuario usuario);
-
-    Optional<Aluno> findByUsuarioId(int id);
 }
