4d288e69ca1e7c720b3950e16d29662f6f7ee5e0
imigoor <imigorgm@gmail.com>
2025-11-12 22:11:25 -0300

    chore: ajuste no CRUD do professor

--- src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/ProfessorRepository.java
+++ src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/ProfessorRepository.java
@@ -1,9 +1,14 @@
 package br.edu.ifpb.pautax.infrastructure.repositories;
 
+import br.edu.ifpb.pautax.domain.entities.Aluno;
 import br.edu.ifpb.pautax.domain.entities.Professor;
+import br.edu.ifpb.pautax.domain.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor,Integer> {
+    Optional<Professor> findByUsuario(Usuario usuario);
 }
