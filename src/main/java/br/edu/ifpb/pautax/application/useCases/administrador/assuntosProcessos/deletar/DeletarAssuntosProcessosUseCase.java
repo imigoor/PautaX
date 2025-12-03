f523b4b70c3c68b8b0de28a610750b1187180c31
Gustavo Gabriel Batista <gugagabriel56@gmail.com>
2025-12-03 01:04:28 -0300

    Fix: Adicionando validação entre professor e processo

--- src/main/java/br/edu/ifpb/pautax/application/useCases/administrador/assuntosProcessos/deletar/DeletarAssuntosProcessosUseCase.java
+++ src/main/java/br/edu/ifpb/pautax/application/useCases/administrador/assuntosProcessos/deletar/DeletarAssuntosProcessosUseCase.java
@@ -3,6 +3,8 @@ package br.edu.ifpb.pautax.application.useCases.administrador.assuntosProcessos.
 import br.edu.ifpb.pautax.infrastructure.repositories.AssuntoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Service
 @RequiredArgsConstructor
@@ -10,7 +12,21 @@ public class DeletarAssuntosProcessosUseCase implements IDeletarAssuntosProcesso
     public final AssuntoRepository assuntoRepository;
 
     @Override
-    public String execute(Integer id) {
+    @Transactional
+    public String execute(Integer id, RedirectAttributes redirectAttributes) {
+        var assunto = assuntoRepository.findById(id).orElse(null);
+
+        if (assunto == null) {
+            redirectAttributes.addFlashAttribute("erro", "Assunto não encontrado.");
+            return "redirect:/admin/assuntos";
+        }
+
+        if (assunto.getProcessos() != null && !assunto.getProcessos().isEmpty()) {
+            redirectAttributes.addFlashAttribute("erro",
+                    "Não é possível excluir: existem processos vinculados a este assunto.");
+            return "redirect:/admin/assuntos";
+        }
+
         assuntoRepository.deleteById(id);
 
         return "redirect:/admin/assuntos";
