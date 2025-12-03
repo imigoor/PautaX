f048eded6ba942e7f64ec961308f2bdf7734d66e
imigoor <imigorgm@gmail.com>
2025-12-03 00:47:53 -0300

    refactor: ajustando para bootstrap

--- src/main/java/br/edu/ifpb/pautax/application/useCases/administrador/professores/deletar/DeletarProfessorUseCase.java
+++ src/main/java/br/edu/ifpb/pautax/application/useCases/administrador/professores/deletar/DeletarProfessorUseCase.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.pautax.application.useCases.administrador.professores.deletar;
 
+import br.edu.ifpb.pautax.infrastructure.repositories.ColegiadoRepository;
 import br.edu.ifpb.pautax.infrastructure.repositories.ProfessorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DeletarProfessorUseCase implements IDeletarProfessorUseCase{
     private final ProfessorRepository professorRepository;
+    private final ColegiadoRepository colegiadoRepository;
 
     @Override
     public String execute(int id) {
+        var professor = professorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+
+        var colegiados = colegiadoRepository.findAllByMembros_Id(id);
+
+        colegiados.forEach(colegiado -> {
+            colegiado.getMembros().remove(professor);
+            colegiadoRepository.save(colegiado);
+        });
+
         professorRepository.deleteById(id);
         return "redirect:/admin/professores";
     }
--- src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/ColegiadoRepository.java
+++ src/main/java/br/edu/ifpb/pautax/infrastructure/repositories/ColegiadoRepository.java
@@ -3,5 +3,8 @@ package br.edu.ifpb.pautax.infrastructure.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifpb.pautax.domain.entities.Colegiado;
 
+import java.util.List;
+
 public interface ColegiadoRepository extends JpaRepository<Colegiado, Integer> {
+    List<Colegiado> findAllByMembros_Id(int id);
 }
